cec88f2bf05b8149968446c241928c8786dc0589
HasiniBalabommu <hasini1607@gmail.com>
2025-04-10 18:54:11 +0530

    Create 3.List-1.py

---
+++ 3.List-1.py
@@ -0,0 +1,26 @@
+justice_league=["superman","batman","wonderwoman","flash","aquaman","Green Lantern"]
+print(len(justice_league))
+justice_league=["superman","batman","wonderwoman","flash","aquaman","Green Lantern"]
+justice_league.append("batgirl")
+justice_league.append("nightwing")
+print(justice_league)
+justice_league=["superman","batman","wonderwoman","flash","aquaman","Green Lantern"]
+justice_league.pop(2)
+justice_league.insert(0,"wonderwoman")
+print(justice_league)
+justice_league=["superman","batman","wonderwoman","flash","aquaman","Green Lantern"]
+justice_league.pop(2)
+justice_league.insert(0,"wonderwoman")
+print(justice_league)
+justice_league=["superman","batman","wonderwoman","flash","aquaman","Green Lantern"]
+justice_league.pop(0)
+justice_league.insert(3,"superman")
+print(justice_league)
+justice_league=["superman","batman","wonderwoman","flash","aquaman","Green Lantern"]
+justice_league1=["Cyborg","Shazam","Hawkgirl","MartianManhunter","Green Arrow"]
+justice_league[:] = justice_league1
+print(justice_league)
+justice_league=["superman","batman","wonderwoman","flash","aquaman","green Lantern"]
+justice_league.sort()
+print(justice_league)
+print("The new leader is:",justice_league[0])
